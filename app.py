4e6a5135c9b068a07c1bb76b02b6a72bb19c068a
sandeepsai15634 <sandeepsai15634@gmai.com>
2026-03-02 12:09:08 +0530

    Fixed text splitter import for Streamlit Cloud

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tempfile
 import streamlit as st
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
